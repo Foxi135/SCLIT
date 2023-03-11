b06045627abd74081e78bc09172b8a318542f580
Foxi135 <118462617+Foxi135@users.noreply.github.com>
2023-03-11 15:17:57 +0100

    test

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import os
 import requests
 import json
 import zipfile
+import datetime
 from colorama import Fore, Style, Back, just_fix_windows_console
 just_fix_windows_console()
-action = sys.argv[1].lower()
+
+try:
+    action = sys.argv[1].lower()
+except:
+    print("next time do \"sclit help\"")
+    quit()
 if (action == "sui"):
     if (sys.argv[2] == "u")or(sys.argv[2] == "i"):
         if sys.argv[2] == "u":
@@ -26,21 +32,64 @@ if (action == "sui"):
 if (action == "download") or (action == "d"):
     id = sys.argv[2]
     x = json.loads((requests.get("https://api.scratch.mit.edu/projects/"+id)).text)
+    token = False
     try:
-        x = x["project_token"]
+        token = sys.argv[sys.argv.index("token:")+1]
     except:
-        print("project with id " + Style.BRIGHT + id + " " + Style.RESET_ALL + Back.RED + "does not exist" + Style.RESET_ALL)
-        quit()
-    x = (requests.get("https://projects.scratch.mit.edu/"+id+"?token="+x)).text
+        token = False
+    if not token:
+        try:
+            newline = "\n"
+            date = datetime.datetime.now()
+            try:
+                d = x["description"]
+                d = None
+            except:
+                x["description"] = ""
+            try:
+                d = x["instructions"]
+                d = None
+            except:
+                x["instructions"] = ""
+            info = f'date of download: {date.year}-{date.month}-{date.day} {date.hour}:{date.minute} {newline}project name: {x["title"]} {newline}created by: {x["author"]["username"]} {newline}project id: {x["id"]} {newline}date of creation: {x["history"]["created"]} {newline}shared: {x["history"]["shared"]} {newline}Instructions:{newline}{x["instructions"]}{newline}Notes and credits (description):{newline}{x["description"]}{newline}{newline}Downloaded with SCLIT by Foxi135'
+            date = None
+            title = x["title"]
+            x = x["project_token"]
+        except:
+            try:
+                u = sys.argv[sys.argv.index("json-url:")+1]
+                u = None
+                newline = "\n"
+                date = datetime.datetime.now()
+                info = f'date of download: {date.year}-{date.month}-{date.day} {date.hour}:{date.minute} {newline}Downloaded with SCLIT by Foxi135'
+                date = None
+            except:
+                print("project with id " + Style.BRIGHT + id + " " + Style.RESET_ALL + Back.RED + "does not exist" + Style.RESET_ALL)
+                quit()
+    else:
+        x = token
+        token = None
     try:
-        sys.argv[3]
+        dir = sys.argv[sys.argv.index("name:")+1]
+        if dir == "?":
+            dir = title
+        for char in (["\\","/",":","*","\"","<",">","|","?"]):
+            dir = dir.replace(char, "_")
+        name = dir+".sb3"
+        dir = "project"+id+" - "+name
     except:
         dir = "project"+id
-    name = dir+".sb3"
+        name = dir+".sb3"
+    try:
+        x = sys.argv[sys.argv.index("json-url:")+1]
+        x = (requests.get(x)).text
+    except:
+        x = (requests.get("https://projects.scratch.mit.edu/"+id+"?token="+x)).text
+    x = json.loads(x)
     if not os.path.exists(dir):
         os.mkdir(dir)
-        open(dir+"/project.json","w").write(x)
-    x = json.loads(x)
+        x["targets"][1]["comments"]["SCLIT-credit"] = {"blockId":None,"x":300,"y":300,"width":200,"height":200,"minimized":False,"text":info}
+        open(dir+"/project.json","w").write(json.dumps(x))
     all = 0
     for w in x["targets"]:
         for v in w["costumes"]:
@@ -78,7 +127,14 @@ if (action == "download") or (action == "d"):
 if action == "help":
     print(
 """
-d/download ━━ <project id> ━━ <file name (optional)>
-sui ━━━━━━━┳━ i (get id) ━━ <username>
+d/download ━━ <project id> ━━ name: <file name or ? to get project title> (optional) ━━ token: <token> (optional) ━━ json-url: <json-url> (optional)
+
+sui ━━━━━━━┳━ i (get id) ━━━━━━━━ <username>
            ┗━ u (get username) ━━ <user id>
+
+examples: 
+sclit d 817400236 name: ?
+sclit sui i Foxi135
+
+created by Foxi135
 """)
